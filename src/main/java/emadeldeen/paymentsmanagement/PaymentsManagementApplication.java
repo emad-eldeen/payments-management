69ac4f2626e7be58d8d3409b45fd907dde889237
emad-eldeen <emadhusameldeen@gmail.com>
2023-05-23 11:28:18 +0200

    init

---
+++ src/main/java/emadeldeen/paymentsmanagement/PaymentsManagementApplication.java
@@ -0,0 +1,13 @@
+package emadeldeen.paymentsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsManagementApplication.class, args);
+	}
+
+}
